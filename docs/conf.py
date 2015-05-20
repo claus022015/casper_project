4f1fd1350904a0a21e1f8e8bcb3b16ed9a16181d
claus022015 <zoica.crisan@gmail.com>
2015-05-20 17:06:56 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,8 @@ from datetime import date
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.extlinks', 'sphinxcontrib.jsdemo']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.extlinks']
 
-extensions.append('sphinxcontrib.jsdemo')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
